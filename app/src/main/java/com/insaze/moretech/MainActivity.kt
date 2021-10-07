d3a11c59401b12cabc1e40652545d8cfcacb8d4a
ulyanovskk <mr1english4@gmail.com>
2021-10-07 16:34:26 +0300

    Add VtbKitFragment

--- app/src/main/java/com/insaze/moretech/MainActivity.kt
+++ app/src/main/java/com/insaze/moretech/MainActivity.kt
@@ -2,9 +2,15 @@ package com.insaze.moretech
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
 
-class MainActivity : AppCompatActivity(R.layout.activity_main) {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentNavHost) as NavHostFragment
+        val navController = navHostFragment.navController
+        navController.popBackStack(R.id.vtbKitFragment, true)
     }
 }
