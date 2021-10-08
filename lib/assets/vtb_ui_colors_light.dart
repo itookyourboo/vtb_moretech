39be91f936fb767aa12b2c80e8da28c33d4a2758
ulyanovskk <mr1english4@gmail.com>
2021-10-08 21:48:00 +0300

    Add light colors

---
+++ lib/assets/vtb_ui_colors_light.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+const blue10 = Color(0xFFEBF3FE);
+const blue20 = Color(0xFFD8E6FC);
+const blue30 = Color(0xFFB0CDF9);
+const blue40 = Color(0xFF89B5F7);
+const blue50 = Color(0xFF629CF4);
+const blue60 = Color(0xFF316FCC);
+const blue70 = Color(0xFF316FCC);
+const blue80 = Color(0xFF225094);
+const blue90 = Color(0xFF14315C);
+const blue100 = Color(0xFF0B1D37);
