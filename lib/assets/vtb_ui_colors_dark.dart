5c63fdbccb87c6bff9e971fabf70e389cb07c9d7
ulyanovskk <mr1english4@gmail.com>
2021-10-08 22:10:40 +0300

    Add dark colors

---
+++ lib/assets/vtb_ui_colors_dark.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+
+const blue10 = Color(0xFF172030);
+const blue20 = Color(0xFF1B2B46);
+const blue30 = Color(0xFF234171);
+const blue40 = Color(0xFF2B579B);
+const blue50 = Color(0xFF326DC6);
+const blue60 = Color(0xFF3A83F1);
+const blue70 = Color(0xFF5C97F1);
+const blue80 = Color(0xFF7EABF0);
+const blue90 = Color(0xFFA0C0EF);
+const blue100 = Color(0xFFC2D4EF);
+
