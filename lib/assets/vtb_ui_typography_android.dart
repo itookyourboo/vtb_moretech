6dc81c6f0307a93a8d0cff63a9260c63234b0e91
ulyanovskk <mr1english4@gmail.com>
2021-10-08 22:54:28 +0300

    Add android typography?

---
+++ lib/assets/vtb_ui_typography_android.dart
@@ -0,0 +1,256 @@
+import 'package:flutter/cupertino.dart';
+
+
+// Be careful, not text align here :|
+const largeTitle1 = TextStyle(
+  fontSize: 32.0,
+  fontStyle: FontStyle.normal,
+  fontWeight: FontWeight.w700,
+  height: 1.25,
+  letterSpacing: 0.0,
+);
+
+const largeTitle2 = TextStyle(
+  fontSize: 28.0,
+  fontStyle: FontStyle.normal,
+  fontWeight: FontWeight.w700,
+  height: 32.0/28.0,
+  letterSpacing: 0.0,
+);
+
+const title1 = TextStyle(
+//  font-family: Roboto;
+// font-size: 16px;
+// font-style: normal;
+// font-weight: 400;
+// line-height: 20px;
+// letter-spacing: 0.25px;
+// text-align: left;
+
+  fontSize: 16.0,
+  fontStyle: FontStyle.normal,
+  fontWeight: FontWeight.w400,
+  height: 1.25,
+  letterSpacing: 0.25,
+);
+
+const title2 = TextStyle(
+  //styleName: Default/Title_2;
+//   font-family: Roboto;
+// font-size: 22px;
+// font-style: normal;
+// font-weight: 500;
+// line-height: 28px;
+// letter-spacing: 0px;
+// text-align: left;
+
+  fontSize: 22.0,
+  fontStyle: FontStyle.normal,
+  fontWeight: FontWeight.w500,
+  height: 28.0/22.0,
+  letterSpacing: 0.0,
+);
+
+const subtitle1 = TextStyle(
+//styleName: Default/Subtitle_1;
+//   font-family: Roboto;
+// font-size: 18px;
+// font-style: normal;
+// font-weight: 700;
+// line-height: 24px;
+// letter-spacing: 0px;
+// text-align: left;
+
+  fontSize: 18.0,
+  fontStyle: FontStyle.normal,
+  fontWeight: FontWeight.w700,
+  height: 24.0/18.0,
+  letterSpacing: 0.0,
+);
+
+const subtitle2 = TextStyle(
+//styleName: Default/Subtitle_2;
+//   font-family: Roboto;
+// font-size: 18px;
+// font-style: normal;
+// font-weight: 500;
+// line-height: 24px;
+// letter-spacing: 0px;
+// text-align: left;
+
+  fontSize: 18.0,
+  fontStyle: FontStyle.normal,
+  fontWeight: FontWeight.w500,
+  height: 24.0/18.0,
+  letterSpacing: 0.0,
+);
+
+const subtitle3 = TextStyle(
+//styleName: Default/Subtitle_3;
+//   font-family: Roboto;
+// font-size: 18px;
+// font-style: normal;
+// font-weight: 400;
+// line-height: 24px;
+// letter-spacing: 0px;
+// text-align: left;
+
+  fontSize: 18.0,
+  fontStyle: FontStyle.normal,
+  fontWeight: FontWeight.w400,
+  height: 24.0/18.0,
+  letterSpacing: 0.0,
+);
+
+const headline = TextStyle(
+//styleName: Default/Headline;
+//   font-family: Roboto;
+// font-size: 16px;
+// font-style: normal;
+// font-weight: 500;
+// line-height: 24px;
+// letter-spacing: 0px;
+// text-align: left;
+
+  fontSize: 16.0,
+  fontStyle: FontStyle.normal,
+  fontWeight: FontWeight.w500,
+  height: 1.5,
+  letterSpacing: 0.0,
+);
+
+const body1 = TextStyle(
+//styleName: Default/Body_1;
+//   font-family: Roboto;
+// font-size: 16px;
+// font-style: normal;
+// font-weight: 500;
+// line-height: 24px;
+// letter-spacing: 0.5px;
+// text-align: left;
+
+  fontSize: 16.0,
+  fontStyle: FontStyle.normal,
+  fontWeight: FontWeight.w700,
+  height: 1.5,
+  letterSpacing: 0.5,
+);
+
+const body2 = TextStyle(
+//styleName: Default/Body_2;
+//   font-family: Roboto;
+// font-size: 16px;
+// font-style: normal;
+// font-weight: 400;
+// line-height: 24px;
+// letter-spacing: 0.25px;
+// text-align: left;
+
+  fontSize: 16.0,
+  fontStyle: FontStyle.normal,
+  fontWeight: FontWeight.w400,
+  height: 1.5,
+  letterSpacing: 0.25,
+);
+
+const subhead1 = TextStyle(
+//styleName: Default/Subhead_1;
+//   font-family: Roboto;
+// font-size: 14px;
+// font-style: normal;
+// font-weight: 700;
+// line-height: 16px;
+// letter-spacing: 0px;
+// text-align: left;
+
+  fontSize: 14.0,
+  fontStyle: FontStyle.normal,
+  fontWeight: FontWeight.w700,
+  height: 16.0/14.0,
+  letterSpacing: 0.0,
+);
+
+const subhead2 = TextStyle(
+//styleName: Default/Subhead_2;
+//   font-family: Roboto;
+// font-size: 14px;
+// font-style: normal;
+// font-weight: 500;
+// line-height: 16px;
+// letter-spacing: 0px;
+// text-align: left;
+
+  fontSize: 14.0,
+  fontStyle: FontStyle.normal,
+  fontWeight: FontWeight.w500,
+  height: 16.0/14.0,
+  letterSpacing: 0.0,
+);
+
+const subhead3 = TextStyle(
+//styleName: Default/Subhead 3;
+//   font-family: Roboto;
+// font-size: 14px;
+// font-style: normal;
+// font-weight: 400;
+// line-height: 20px;
+// letter-spacing: 0px;
+// text-align: left;
+
+  fontSize: 14.0,
+  fontStyle: FontStyle.normal,
+  fontWeight: FontWeight.w400,
+  height: 20.0/14.0,
+  letterSpacing: 0.0,
+);
+
+const caption1 = TextStyle(
+//styleName: Default/Caption_1;
+//   font-family: Roboto;
+// font-size: 12px;
+// font-style: normal;
+// font-weight: 500;
+// line-height: 16px;
+// letter-spacing: 0.4000000059604645px;
+// text-align: left;
+
+  fontSize: 12.0,
+  fontStyle: FontStyle.normal,
+  fontWeight: FontWeight.w500,
+  height: 16.0/12.0,
+  letterSpacing: 0.4000000059604645,
+);
+
+const caption2 = TextStyle(
+//styleName: Default/Caption_2;
+//   font-family: Roboto;
+// font-size: 10px;
+// font-style: normal;
+// font-weight: 500;
+// line-height: 12px;
+// letter-spacing: 0.4000000059604645px;
+// text-align: left;
+
+  fontSize: 10.0,
+  fontStyle: FontStyle.normal,
+  fontWeight: FontWeight.w500,
+  height: 1.2,
+  letterSpacing: 0.40000000596046450,
+);
+
+const overLine = TextStyle(
+//styleName: Default/OVERLINE;
+//   font-family: Roboto;
+// font-size: 10px;
+// font-style: normal;
+// font-weight: 500;
+// line-height: 12px;
+// letter-spacing: 0.5px;
+// text-align: left;
+
+  fontSize: 10.0,
+  fontStyle: FontStyle.normal,
+  fontWeight: FontWeight.w500,
+  height: 1.2,
+  letterSpacing: 0.5,
+);
