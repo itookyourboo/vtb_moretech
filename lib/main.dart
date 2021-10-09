429f1506a98127135e208e24f275bedf50fc9f37
itookyourboo <Wignorbo@yandex.ru>
2021-10-10 00:50:14 +0300

    StartScreen + new icons

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:moretech_vtb/screen/games_page.dart';
+import 'package:moretech_vtb/screen/intro_screen.dart';
 import 'package:moretech_vtb/screen/main_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+
+}
+
+class _MyAppState extends State<MyApp> {
+  static SharedPreferences? prefs;
+  bool passed = false;
+
+  @override
+  void initState() {
+    super.initState();
+    initPreferences().whenComplete((){
+      setState(() {
+        passed = hasPassed();
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +37,25 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MainScreen(),
+      home: !passed ? IntroScreen((name, age){
+        setState(() {
+          passed = true;
+          prefs!.setString("name", name);
+          prefs!.setInt("age", age);
+          prefs!.setBool("passed_intro", true);
+        });
+      }): const MainScreen(),
     );
+  }
+
+  Future<void> initPreferences() async {
+    prefs = await SharedPreferences.getInstance();
+  }
+
+  bool hasPassed() {
+    if (prefs != null) {
+      return prefs!.containsKey("passed_intro");
+    }
+    return false;
   }
 }
---
+++ lib/screen/intro_screen.dart
@@ -0,0 +1,226 @@
+import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:moretech_vtb/assets/vtb_ui_typography.dart' as VtbTypography;
+
+class IntroScreen extends StatefulWidget {
+  final Function(String name, int age) passed;
+
+  const IntroScreen(this.passed, {Key? key}) : super(key: key);
+
+  @override
+  _IntroScreenState createState() => _IntroScreenState();
+}
+
+class _IntroScreenState extends State<IntroScreen> {
+  List<List<String>> texts = [
+    [
+      'Привет!',
+      'Меня зовут Вика!\nЯ буду твоим финансовым наставником!',
+      'Никаких скучных тестов и кучи теории.',
+      'Изучай финансовую грамотность играючи!',
+      'Для начала ответь на несколько простых вопросов.',
+      'Как тебя зовут?'
+    ],
+    [
+      'Приятно познакомиться, ',
+      'Укажи свой возраст, чтобы я подобрал подходящую программу.'
+    ],
+    ['Да это же самые лучшие годы!', 'Ну что, начнем!']
+  ];
+  int _stage = 0;
+  late String _name;
+  late int _age;
+  late List<Widget> children;
+
+  @override
+  void initState() {
+    super.initState();
+    children = [getAnimatedTextKitByStage(0)];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+            padding: const EdgeInsets.all(16.0),
+            alignment: Alignment.center,
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: children)));
+  }
+
+  AnimatedTextKit getAnimatedTextKitByStage(int stage) {
+    return AnimatedTextKit(
+      key: ValueKey<int>(stage),
+      isRepeatingAnimation: false,
+      displayFullTextOnTap: true,
+      animatedTexts: List.of(texts[stage].map((text) => TyperAnimatedText(text,
+          speed: Duration(milliseconds: 50),
+          textAlign: TextAlign.center,
+          textStyle: VtbTypography.body1))),
+      onFinished: () {
+        setState(() {
+          print('added form ' + stage.toString());
+          children.add(getFormByStage(stage));
+        });
+      },
+    );
+  }
+
+  Widget getFormByStage(stage) {
+    if (stage == 0) {
+      return NameForm(entered: (input) {
+        setState(() {
+          _stage = 1;
+          _name = input;
+          texts[1][0] += '$_name!';
+          children = [getAnimatedTextKitByStage(_stage)];
+        });
+      });
+    } else if (stage == 1) {
+      return AgeForm(entered: (input) {
+        setState(() {
+          _stage = 2;
+          _age = input;
+          children = [getAnimatedTextKitByStage(_stage)];
+        });
+      });
+    } else if (stage == 2) {
+      return Container(
+          margin: const EdgeInsets.only(top: 8.0),
+          width: MediaQuery.of(context).size.width,
+          child: ElevatedButton(
+              onPressed: () {
+                widget.passed(_name, _age);
+              },
+              child: const Text('Поехали!')));
+    } else {
+      return Text('That`s all');
+    }
+  }
+
+  Widget getContentByStage(int stage) {
+    return Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: children);
+  }
+}
+
+class NameForm extends StatefulWidget {
+  final Function(String) entered;
+
+  const NameForm({required this.entered, Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _NameFormState();
+}
+
+class _NameFormState extends State<NameForm> {
+  final textController = TextEditingController();
+
+  @override
+  void dispose() {
+    textController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            TextField(
+              controller: textController,
+            ),
+            Container(
+              child: ElevatedButton(
+                  child: Text('OK'),
+                  onPressed: () {
+                    String input = textController.text;
+                    if (isCorrectName(input)) {
+                      widget.entered(input[0].toUpperCase() +
+                          input.substring(1).toLowerCase());
+                    } else {
+                      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                          content: Text('Пожалуйста, введите имя!')));
+                    }
+                  }),
+              margin: const EdgeInsets.only(top: 8.0),
+              width: MediaQuery.of(context).size.width,
+            )
+          ],
+        ));
+  }
+
+  bool isCorrectName(String name) {
+    return RegExp(r'[а-яА-Я\w]{2,}').hasMatch(name);
+  }
+}
+
+class AgeForm extends StatefulWidget {
+  final Function(int) entered;
+
+  const AgeForm({required this.entered, Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _AgeFormState();
+}
+
+class _AgeFormState extends State<AgeForm> {
+  final textController = TextEditingController();
+
+  @override
+  void dispose() {
+    textController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            TextField(
+              controller: textController,
+            ),
+            Container(
+              child: ElevatedButton(
+                  child: Text('OK'),
+                  onPressed: () {
+                    String input = textController.text;
+                    int age = isCorrectAge(input);
+                    if (age != 0) {
+                      widget.entered(age);
+                    } else {
+                      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                          content: Text('Пожалуйста, введите возраст!')));
+                    }
+                  }),
+              margin: const EdgeInsets.only(top: 8.0),
+              width: MediaQuery.of(context).size.width,
+            )
+          ],
+        ));
+  }
+
+  int isCorrectAge(String input) {
+    try {
+      int num = int.parse(input);
+      if (0 < num && num < 120) {
+        return num;
+      }
+      return 0;
+    } catch (err) {
+      return 0;
+    }
+  }
+}
